5887695b71ccb43210a0ba78d6284e810f84f68e
Lancerfx <yt0328575@gmail.com>
2025-10-26 18:48:41 +0545

    all for pushing first time with some backend code

---
+++ backend/controller/productController.js
@@ -0,0 +1,57 @@
+import Product from "../models/productModel.js"
+
+//1️⃣Creating Products
+export const createProducts=async(req,res)=>{
+  const product =await Product.create(req.body)
+  res.status(201).json({
+    success:true,
+    product
+  })
+}
+
+//2️⃣Get all the products
+export const getAllProducts=async(req,res)=>{
+  const products=await Product.find()
+  res.status(200).json({
+    success:true,
+    products
+  })
+  } 
+
+  //3️⃣ Update Product
+
+  export const updateProduct=async(req,res)=>{
+    let product=await Product.findById(req.params.id);
+    if(!product){
+      return res.status(500).json({
+        success:false,
+        message:"Product Not found"
+      })
+    }
+
+    product=await Product.findByIdAndUpdate(req.params.id,req.body,{
+      new:true,
+      runValidators:true
+    })
+    res.status(200).json({
+      success:true,
+      product
+    })
+  }
+
+  //4️⃣ Delete Product
+
+  export const deleteProduct = async(req,res)=>{
+    let product=await Product.findById(req.params.id);
+    if(!product){
+      return res.status(500).json({
+        success:false,
+        message:"Product Not found"
+      })
+    }
+    product=await Product.findByIdAndDelete(req.params.id)
+    res.status(200).json({
+      success:true,
+      message:"Product Deleted Successfully"
+    })
+  }
